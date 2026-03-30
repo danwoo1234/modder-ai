5abf2677e54c98ecd1d14383c3ca6e4f2fee988d
엄단우 <davideom0414@gmail.com>
2026-03-30 15:17:23 +0000

    Fix error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,6 +8,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
   callbacks: {
     async session({ session }) {
       return session;
